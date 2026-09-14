48b388d434c0a512dc3aa4dec1b28269e99a4d70
Takomon <takomon2010@gmail.com>
2026-09-14 21:54:24 +0900

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/emyureta/' : '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+});
